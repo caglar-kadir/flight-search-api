9bf9aaf5990c548d9e5b6c0dc4d254d40ac41bd2
TREND-TECH\kadir.caglar <kadir.caglar@payguru.com>
2024-05-10 17:21:36 +0300

    Create Project with Spring Initializer

---
+++ src/test/java/org/caglar/flightsearchapi/FlightSearchApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.caglar.flightsearchapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlightSearchApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

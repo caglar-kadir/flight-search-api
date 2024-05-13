4ea961fec7a230a45a7da842910c454e7c96ad49
TREND-TECH\kadir.caglar <kadir.caglar@payguru.com>
2024-05-13 15:31:42 +0300

    Create exceptions for date and price

---
+++ src/main/java/org/caglar/flightsearchapi/exceptions/InvalidDateException.java
@@ -0,0 +1,7 @@
+package org.caglar.flightsearchapi.exceptions;
+
+public class InvalidDateException extends Exception{
+    public InvalidDateException(String message){
+        super(message);
+    }
+}

4de90888aa71b4aad0f6a3ca08c69467005755df
TREND-TECH\kadir.caglar <kadir.caglar@payguru.com>
2024-05-13 15:30:31 +0300

    Create save method for flights on flight controller

--- src/main/java/org/caglar/flightsearchapi/controller/FlightController.java
+++ src/main/java/org/caglar/flightsearchapi/controller/FlightController.java
@@ -1,13 +1,23 @@
 package org.caglar.flightsearchapi.controller;
 
+import org.caglar.flightsearchapi.models.Flight;
 import org.caglar.flightsearchapi.service.FlightService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/flight-search/v1")
 public class FlightController {
     FlightService flightService;
 
-    FlightController (FlightService flightService) {
+    FlightController(FlightService flightService) {
         this.flightService = flightService;
+    }
+
+    @PostMapping
+    public Flight save(@RequestBody Flight flight) throws Exception {
+        return flightService.save(flight);
     }
 }

e568db2accd498d49ca4a8be3ab4c8f3e07e5c51
TREND-TECH\kadir.caglar <kadir.caglar@payguru.com>
2024-05-13 15:28:28 +0300

    create save method on flight repository

--- src/main/java/org/caglar/flightsearchapi/repository/FlightRepository.java
+++ src/main/java/org/caglar/flightsearchapi/repository/FlightRepository.java
@@ -4,4 +4,5 @@ import org.caglar.flightsearchapi.models.Flight;
 import org.springframework.data.repository.Repository;
 
 public interface FlightRepository extends Repository<Flight, Long> {
+    Flight save(Flight flight);
 }

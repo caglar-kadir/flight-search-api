d5436f97e23d82005fbc5381ec0105f5bad20cc4
TREND-TECH\kadir.caglar <kadir.caglar@payguru.com>
2024-05-14 14:35:43 +0300

    move isFlightValid method to util class

--- src/main/java/org/caglar/flightsearchapi/service/FlightService.java
+++ src/main/java/org/caglar/flightsearchapi/service/FlightService.java
@@ -18,7 +18,7 @@ public class FlightService {
     }
 
     public Flight save(Flight flight) throws Exception {
-        if(isFlightValid(flight)){
+        if(DateUtil.isFlightValid(flight)){
             return flightRepository.save(flight);
         }
         throw new Exception("Unsuccessful process.");
@@ -26,26 +26,11 @@ public class FlightService {
     public List<Flight> saveAll(List<Flight> flights) {
         return flights.stream().filter(flight -> {
             try {
-                return isFlightValid(flight);
+                return DateUtil.isFlightValid(flight);
             } catch (InvalidDateException | InvalidPriceException e) {
                 throw new RuntimeException(e);
             }
         }).map(flight -> flightRepository.save(flight)).toList();
     }
 
-    private boolean isFlightValid(Flight flight) throws InvalidDateException, InvalidPriceException {
-        if(flight.getDepartureDate().isEmpty() || flight.getArrivalDate().isEmpty()){
-            throw new InvalidDateException("Date cannot be null.");
-        }
-        if (!DateUtil.isValidDate(flight.getDepartureDate()) || !DateUtil.isValidDate(flight.getArrivalDate())) {
-            throw new InvalidDateException("Invalid date.");
-        }
-        if(!DateUtil.isDepartureDateAfterArrivalDate(flight.getDepartureDate(), flight.getArrivalDate())){
-            throw new InvalidDateException("Departure date cannot be later than arrival date");
-        }
-        if (flight.getPrice() < 0) {
-            throw new InvalidPriceException("Invalid price.");
-        }
-        return true;
-    }
 }

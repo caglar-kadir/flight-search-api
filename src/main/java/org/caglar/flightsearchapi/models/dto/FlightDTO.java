cf7dc96300cdb9634a75235a39dffbbb80f52e08
TREND-TECH\kadir.caglar <kadir.caglar@payguru.com>
2024-05-15 11:22:48 +0300

    convert model to DTO object

--- src/main/java/org/caglar/flightsearchapi/models/dto/FlightDTO.java
+++ src/main/java/org/caglar/flightsearchapi/models/dto/FlightDTO.java
@@ -3,10 +3,12 @@ package org.caglar.flightsearchapi.models.dto;
 import lombok.Data;
 import org.caglar.flightsearchapi.models.Airport;
 
+import java.time.LocalDateTime;
+
 @Data
 public class FlightDTO {
-    private String departureDate;
-    private String arrivalDate;
+    private LocalDateTime departureDate;
+    private LocalDateTime arrivalDate;
     private double price;
     private Airport departureAirport;
     private Airport arrivalAirport;
--- src/main/java/org/caglar/flightsearchapi/service/FlightService.java
+++ src/main/java/org/caglar/flightsearchapi/service/FlightService.java
@@ -2,32 +2,41 @@ package org.caglar.flightsearchapi.service;
 
 import org.caglar.flightsearchapi.exceptions.InvalidDateException;
 import org.caglar.flightsearchapi.exceptions.InvalidPriceException;
+import org.caglar.flightsearchapi.exceptions.airportExceptions.AirportNotFoundException;
 import org.caglar.flightsearchapi.models.Flight;
+import org.caglar.flightsearchapi.models.dto.FlightDTO;
+import org.caglar.flightsearchapi.repository.AirportRepository;
 import org.caglar.flightsearchapi.repository.FlightRepository;
 import org.caglar.flightsearchapi.utils.DateUtil;
+import org.caglar.flightsearchapi.utils.MapperUtil;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class FlightService {
     FlightRepository flightRepository;
+    AirportRepository airportRepository;
 
-    FlightService(FlightRepository flightRepository) {
+    @Autowired
+    FlightService(FlightRepository flightRepository, AirportRepository airportRepository) {
         this.flightRepository = flightRepository;
+        this.airportRepository = airportRepository;
     }
 
-    public Flight save(Flight flight) throws Exception {
-        if(flight.getDepartureDate().isEmpty() || flight.getArrivalDate().isEmpty()){
-            throw new InvalidDateException("Date cannot be null.");
+    public FlightDTO save(FlightDTO flightDTO, String departureAirport, String arrivalAirport) throws InvalidDateException, InvalidPriceException, AirportNotFoundException {
+
+        flightDTO.setDepartureAirport(airportRepository.findByCityCode(departureAirport));
+        flightDTO.setArrivalAirport(airportRepository.findByCityCode(arrivalAirport));
+
+        if(flightDTO.getDepartureAirport() != null && flightDTO.getArrivalAirport() != null){
+            Flight flight = MapperUtil.instance().map(flightDTO, Flight.class);
+            if(flight != null){
+                if(DateUtil.isFlightValid(flight)){
+                    flightRepository.save(flight);
+                    return flightDTO;
+                }
+            }
         }
-        if (!DateUtil.isValidDate(flight.getDepartureDate()) || !DateUtil.isValidDate(flight.getArrivalDate())) {
-            throw new InvalidDateException("Invalid date.");
-        }
-        if(!DateUtil.isDepartureDateAfterArrivalDate(flight.getDepartureDate(), flight.getArrivalDate())){
-            throw new InvalidDateException("Departure date cannot be later than arrival date");
-        }
-        if (flight.getPrice() < 0) {
-            throw new InvalidPriceException("Invalid price.");
-        }
-        return flightRepository.save(flight);
+        throw new AirportNotFoundException("Invalid airport name: " + departureAirport + " - " + arrivalAirport);
     }
 }
